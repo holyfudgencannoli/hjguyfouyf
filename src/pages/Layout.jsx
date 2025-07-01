1e2ee716f332d0132c893bf037b07c101112bbd0
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-06-30 22:13:58 -0400

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,11 @@
 import { Link, Outlet } from 'react-router-dom';
 import '../pages/Layout.css';
-import '../components/sidebar/Sidebar.jsx'
+import AppBar from '@mui/material/AppBar';
 
 export default function Layout() {
   return (
     <>
+      <AppBar />
       <main className='content'>
         <Outlet /> {/* This is where nested routes render */}
       </main>
