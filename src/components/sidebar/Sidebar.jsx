44e1c8d893e3cd2c15eab58ca023eda352ee5972
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-06-30 22:25:00 -0400

    Update Sidebar.jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -21,16 +21,6 @@ export default function Sidebar() {
     return(
         <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar
-            position="fixed"
-            sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-        >
-            <Toolbar>
-            <Typography variant="h6" noWrap component="div">
-                Pylogger
-            </Typography>
-            </Toolbar>
-        </AppBar>
         <Drawer
             sx={{
             width: drawerWidth,
