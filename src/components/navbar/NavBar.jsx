b1b9394f148a2cf9b5ee6e4834d2e30626b8c2d4
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-06-30 22:27:36 -0400

    Update NavBar.jsx

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,21 @@
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-
-
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
 
 export default function NavBar{
   return(
-    <AppBar>
-      PYLOGGER
-    </AppBar>
+        <CssBaseline />
+        <AppBar
+            position="fixed"
+            sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+        >
+            <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+                Pylogger
+            </Typography>
+            </Toolbar>
+        </AppBar>
   )
 }
